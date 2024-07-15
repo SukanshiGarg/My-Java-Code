10b71d0c6433095540ecf16e8a1f6b6678440d18
SukanshiGarg <sukanshigargagra123@gmail.com>
2024-07-15 22:15:35 +0530

    first commit

---
+++ OOPS/Person.java
@@ -0,0 +1,16 @@
+package OOPS;
+
+public class Person {
+    int age= 20;
+    String name= "Sukanshi Garg";
+
+    public Person{
+
+    }
+
+    public Person(String name, int age ){
+        this.name= nem;
+        this.age=age;
+    }
+    
+}
