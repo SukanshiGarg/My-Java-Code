eb0fea93a363a8d7d148b24f57715bcbb6e10114
SukanshiGarg <sukanshigargagra123@gmail.com>
2024-07-26 00:45:15 +0530

    first commit

--- MultiLevel_Inheritence_Demo/child.java
+++ MultiLevel_Inheritence_Demo/child.java
@@ -1,5 +1,9 @@
 package MultiLevel_Inheritence_Demo;
 
 public class child extends parent{
+
+    public child(){
+        System.out.println("This is the child constructor");
+    }
     
 }
--- MultiLevel_Inheritence_Demo/parent.java
+++ MultiLevel_Inheritence_Demo/parent.java
@@ -1,5 +1,9 @@
 package MultiLevel_Inheritence_Demo;
 
 public class parent extends grandparent {
+
+    public parent(){
+        System.out.println("Parent Construct Called : ");
+    }
     
 }
