bdfc4483f0edcf7ba3c81442dcc04876778dbf75
SukanshiGarg <sukanshigargagra123@gmail.com>
2024-07-26 00:42:48 +0530

    first commit

--- MultiLevel_Inheritence_Demo/test.java
+++ MultiLevel_Inheritence_Demo/test.java
@@ -1,11 +1,16 @@
+// This code demonstrates multi-level inheritance in Java by creating a child class instance,
+// invoking its method, and observing the constructor calls in the inheritance hierarchy.
+
 package MultiLevel_Inheritence_Demo;
 
 public class test {
 
     public static void main(String[] args) {
-    child ch = new child();
-    System.out.println()
+        child ch = new child();
+        System.out.println(ch.hasSuperPowers());
+
+        // Expected output:
+        // This is the grandparent constructor!
+        // false
     }
-    
-    
 }
