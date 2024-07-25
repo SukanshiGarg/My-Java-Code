30793d6e5082cd57b6a247bc498625c6f0d2ebc0
SukanshiGarg <sukanshigargagra123@gmail.com>
2024-07-26 00:11:52 +0530

    first commit

---
+++ Inheritance1/Dog.java
@@ -0,0 +1,9 @@
+package Inheritance1;
+
+public class Dog extends Animal {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ OOPS.java/CopyCon2Main.java
@@ -0,0 +1,25 @@
+class CopyCon2{
+    int a,b;                           //instance variables 
+
+    CopyCon2(int x,int y){ 
+        //parametrised constructor 
+        a=x;
+        b=y;
+
+    }
+
+    CopyCon2(CopyCon2 p){
+        a=p.a;
+        b=p.b;
+    }
+}
+public class CopyCon2Main {
+    public static void main(String[] args) {
+        CopyCon2 obj1 = new CopyCon2(5,8);
+        CopyCon2 obj2 = new CopyCon2(obj1);
+
+
+        
+    }
+    
+}
