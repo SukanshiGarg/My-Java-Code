3bf70f29e4b8d81430c14382ac4a6eb2824c883f
SukanshiGarg <sukanshigargagra123@gmail.com>
2024-08-01 20:06:52 +0530

    first commit

---
+++ oopsjava/LearnInterfaces.java
@@ -0,0 +1,29 @@
+package oopsjava;
+
+public class LearnInterfaces {
+    public static void main(String args[]) {
+        Monkey monkey = new Monkey();
+        monkey.eats();
+        monkey.sings();
+    }
+}
+
+interface Animal {
+    void eats();
+}
+
+interface Pet {
+    void sings();
+}
+
+class Monkey implements Animal, Pet {
+    @Override
+    public void sings() {
+        System.out.println("Monkey is singing ");
+    }
+
+    @Override
+    public void eats() {
+        System.out.println("Monkey is eating");
+    }
+}
