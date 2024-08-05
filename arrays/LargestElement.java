bb6b925038775d09988e7050feb168a98dc83d25
SukanshiGarg <sukanshigargagra123@gmail.com>
2024-08-05 21:00:00 +0530

    first commit

---
+++ arrays/LargestElement.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class LargestElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x;
+      
+        x = sc.nextInt(); // O(1)
+        int arr[] = new int[x];
+        for (int i = 0; i < x; i++) { // O(n)
+            arr[i] = sc.nextInt();
+        }
+        sc.close(); // Closing the scanner after reading all inputs
+
+        int largest = arr[0];
+        for (int i = 0; i < x; i++) { // O(n)
+            largest = Math.max(largest, arr[i]);
+        }
+        System.out.println(largest); // O(1)
+    }
+}
