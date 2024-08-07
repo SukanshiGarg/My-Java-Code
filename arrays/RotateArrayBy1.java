f7ea456a622ddf36475ce48781bbf2e9fe91b7b0
SukanshiGarg <sukanshigargagra123@gmail.com>
2024-08-07 19:37:33 +0530

    first commit

--- arrays/RotateArrayBy1.java
+++ arrays/RotateArrayBy1.java
@@ -4,7 +4,16 @@
 public class RotateArrayBy1 {
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5,6};
-        
+         //store the first term in an temporary variable 
+         int temp=arr[0];
+         //start a for loop and store all the leading 
+        for(int i=1;i<arr.length;i++){
+            arr[i-1]=arr[i];
+        }
+        arr[arr.length-1]=temp;
+        for(int num:arr){
+            System.out.print(num+" ");
+        }
 
     }
     
