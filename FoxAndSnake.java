a56073650ea702a0b0584db8c97e1e54017617c5
SukanshiGarg <sukanshigargagra123@gmail.com>
2024-08-09 22:01:36 +0530

    first commit

--- FoxAndSnake.java
+++ FoxAndSnake.java
@@ -8,18 +8,23 @@ public class FoxAndSnake {
         String a="";
         String b="";
 
-        for(int i=0;i<m-1;i++){
+        for(int i=0;i<m-1;i++){             // one less the numbeer of last index to match the number 0f dotes 
+                                            // String a=#########
+                                            //String b=........
             a+="#";
             b+=".";
 
             
         }
-        a+="#";
-        boolean flag=true;
+
+        a+="#";     //the remaining #
+        boolean flag=true; 
         for(int i=0;i<n;i++){
-            if(i%2==0){
-                System.out.println(a);
+            if(i%2==0){                        //for even number of rows print #
+                System.out.println(a); 
             }
+
+            //for the odd number of the rows : alternate times mai true /false khud ho jayengi 
             else{
                 if(flag){
                     System.out.println(b+"#");
