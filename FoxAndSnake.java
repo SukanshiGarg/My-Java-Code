c5e356514723ff0bedbacce5484c74899bf7342e
SukanshiGarg <sukanshigargagra123@gmail.com>
2024-08-09 21:49:28 +0530

    first commit

---
+++ FoxAndSnake.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class FoxAndSnake {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int m=sc.nextInt();
+        
+        String a="";
+        String b="";
+
+        for(int i=0;i<m-1;i++){
+            a+="#";
+            b+=".";
+
+            
+        }
+        a+="#";
+        boolean flag=true;
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                System.out.println(a);
+            }
+            else{
+                if(flag){
+                    System.out.println(b+"#");
+                    flag=!flag;
+                }
+                else{
+                    System.out.println("#"+b);
+                    flag=!flag;
+                }
+               
+            }
+        }
+
+        
+    }
+    
+}
