d200e3b16a08eb72d20a2436012dd54fb9eb07d5
SukanshiGarg <sukanshigargagra123@gmail.com>
2024-08-12 23:27:56 +0530

    first commit

---
+++ Cp_1368A_CAgreaterthanB.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Cp_1368A_CAgreaterthanB {
+    
+    // Method to calculate the minimum number of operations
+    public static int minOperations(int a, int b, int n) {
+        int operations = 0;
+        while (a <= n && b <= n) {
+            if (a < b) {
+                a += b;
+            } else {
+                b += a;
+            }
+            operations++;
+        }
+        return operations;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // Read the number of test cases
+        int T = sc.nextInt();
+        int[] results = new int[T];
+
+        // Process each test case
+        for (int i = 0; i < T; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
+            results[i] = minOperations(a, b, n);
+        }
+        
+        // Output the results for each test case
+        for (int result : results) {
+            System.out.println(result);
+        }
+        
+        // Close the scanner
+        sc.close();
+    }
+}
--- Cp_339A_HelpfulMaths.java
+++ Cp_339A_HelpfulMaths.java
@@ -1,9 +1,35 @@
 import java.util.*;
+import java.util.Arrays;
 public class Cp_339A_HelpfulMaths {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s=sc.nextLine();
-        
+        String str=sc.next();
+        String sym="\\+";  
+
+        String[] parts = str.split(sym);
+        int numbers[]=new int[parts.length];
+
+        //covert string parts to integer
+        for(int i=0; i<parts.length ;i++){
+            numbers[i]=Integer.parseInt(parts[i]);
+        }
+
+        //sort the number
+        Arrays.sort(numbers);
+
+        //print the numbers with "+" in between
+        for(int i=0;i<numbers.length;i++){
+            if(i>0){
+                System.out.print("+");
+            }
+            System.out.print(numbers[i]);
+        }
+
+
+
+
+
+
         
     }
     
