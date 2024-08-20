ed11d312610527055c91d56e51aa03cd70fc6b28
SukanshiGarg <sukanshigargagra123@gmail.com>
2024-08-20 19:39:18 +0530

    first commit

--- pcph/removeduplicatesfromarray.java
+++ pcph/removeduplicatesfromarray.java
@@ -1,16 +1,18 @@
 package pcph;
 import java.util.*;
+
 public class removeduplicatesfromarray {
-    public static int[] removeDuplicates( int arr[]){
-        int j=0;
-        for(int i=1;i<arr.length;i++){
-            if(arr[j]!=arr[i]){
+    public static int[] removeDuplicates(int arr[]) {
+        int j = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[j] != arr[i]) {
                 j++;
-                arr[j]=arr[i];
+                arr[j] = arr[i];
             }
         }
-        return Arrays.copyOfRange(arr, 0, j+ 1);;
+        return Arrays.copyOfRange(arr, 0, j + 1);
     }
+
     public static void main(String[] args) {
         int arr[] = {0, 0, 1, 2, 2, 3, 4, 5, 5, 5, 6, 6, 6, 7};
         int ans[] = removeDuplicates(arr);
@@ -19,7 +21,5 @@ public class removeduplicatesfromarray {
         for (int i = 0; i < ans.length; i++) {
             System.out.print(ans[i] + " ");
         }
-
-    
-}
+    }
 }
