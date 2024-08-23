5be638600d410345c7fa13555aa648d02748184a
SukanshiGarg <sukanshigargagra123@gmail.com>
2024-08-23 19:04:02 +0530

    first commit

---
+++ pcph/rem.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class rem {
+    public static int sumOfdigits(int n){
+        int sum=0;
+        while(n>0){
+            int rem=n%10;
+            sum+=rem;
+            n/=10;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int ans=n;
+        while(ans>9){
+            ans=sumOfdigits(ans);
+        }
+        System.out.println(ans);
+
+        
+    }
+    
+}
