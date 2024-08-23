4ec8c7d52f21006c322f5f1f967f7d07a78e0b9b
SukanshiGarg <sukanshigargagra123@gmail.com>
2024-08-23 21:15:58 +0530

    first commit

--- arrays/MissingNumber3.java
+++ arrays/MissingNumber3.java
@@ -1,9 +1,36 @@
+import java.util.*;
 public class MissingNumber3 {
     public static void main(String[] args) {
         //XOR Gate
         //a^a=0
-        
+        //2^2^3^3=0
 
+        //zero xor with any number is the number itself
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+
+
+
+
+    }
+    public static int missingNumber(int arr[],int n){
+        int xor1=0;
+        int xor2=0;
+        for(int i=0;i<n-1;i++){
+            xor2=xor2^arr[i];
+            //xor of all array elements 
+            xor1=xor1^arr[i];
+
+            xor2=xor2^i;
+            //xor upto n-1
+
+        }
+        return xor1^xor2;
     }
     
 }
