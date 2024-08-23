8ff40fe6723ce6fe7d9d80e968103128ee7ea512
SukanshiGarg <sukanshigargagra123@gmail.com>
2024-08-23 21:20:36 +0530

    first commit

--- arrays/MissingNumber3.java
+++ arrays/MissingNumber3.java
@@ -1,36 +1,28 @@
+
+
+
 import java.util.*;
+
 public class MissingNumber3 {
-    public static void main(String[] args) {
-        //XOR Gate
-        //a^a=0
-        //2^2^3^3=0
+    public static int missingNumber(int []a, int N) {
 
-        //zero xor with any number is the number itself
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        int arr[]=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
+        int xor1 = 0, xor2 = 0;
+
+        for (int i = 0; i < N - 1; i++) {
+            xor2 = xor2 ^ a[i]; // XOR of array elements
+            xor1 = xor1 ^ (i + 1); //XOR up to [1...N-1]
         }
+        xor1 = xor1 ^ N; //XOR up to [1...N]
 
-
-
-
-
+        return (xor1 ^ xor2); // the missing number
     }
-    public static int missingNumber(int arr[],int n){
-        int xor1=0;
-        int xor2=0;
-        for(int i=0;i<n-1;i++){
-            xor2=xor2^arr[i];
-            //xor of all array elements 
-            xor1=xor1^arr[i];
 
-            xor2=xor2^i;
-            //xor upto n-1
+    public static void main(String args[]) {
+        int N = 5;
+        int a[] = {1, 2, 4, 5};
 
-        }
-        return xor1^xor2;
+        int ans = missingNumber(a, N);
+        System.out.println("The missing number is: " + ans);
     }
-    
 }
+
