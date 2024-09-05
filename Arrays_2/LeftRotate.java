a28bb2dcc64d9d70fec23bb962a9f360ce5d2035
SukanshiGarg <sukanshigargagra123@gmail.com>
2024-09-05 20:29:45 +0530

    first commit

---
+++ Arrays_2/LeftRotate.java
@@ -0,0 +1,34 @@
+package Arrays_2;
+
+public class LeftRotate {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7};
+        int k=2;
+        leftrotation(arr,k);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void leftrotation(int arr[],int k){
+        int temp[]=new int[k];
+        int n=arr.length;
+
+        //store the k elements in a temporary array
+        for(int i=0;i<k;i++){
+            temp[i]=arr[i];
+        }
+
+        //now, store the n-k elements to starting of the original array
+        for(int i=k;i<arr.length;i++){
+           arr[i-k]=arr[i];
+        }
+
+        //step 3: place the elements of the temporary elements in the last d places of the given array
+
+        for(int i=n-k ; i< arr.length ;i++){
+             arr[i]=temp[i-(n-k)];
+        }
+    }
+    
+}
--- Leetcode/SumOfDigitsAfterConvert.java
+++
@@ -1,9 +0,0 @@
-package Leetcode;
-//s="iiii"
-//k=1 
-public class SumOfDigitsAfterConvert {
-    public int getLucky(String s,int k){
-
-    }
-    
-}
