fd997eb9f7a18097597d2577d16a0b89240899b9
SukanshiGarg <sukanshigargagra123@gmail.com>
2024-10-15 19:09:23 +0530

    first commit

---
+++ TwoSum1.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+public class TwoSum1 {
+    public int[] twoSum(int nums[],int target){
+        //create a hashMap to store the value and its index
+        HashMap<Integer,Integer> map= new HashMap<>();
+
+        //traverse the array
+        for(int i=0;i<nums.length;i++){
+            int complement = target - nums[i];
+        }
+    }
+    public static void main(String[] args) {
+        
+
+    }
+    
+}
